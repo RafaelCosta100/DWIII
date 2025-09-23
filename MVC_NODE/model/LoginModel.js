a831174cc6cacf0873d7067deb512eb3a8e0a5d9
RafaelS-Costa <rafaelsartoricosta@outlook.com>
2025-09-23 20:18:29 -0300

    inclusão de novas tarefas

--- MVC_NODE/model/LoginModel.js
+++ MVC_NODE/model/LoginModel.js
@@ -1,8 +1,8 @@
 //criando a classe LoginModel
-module.exports = class LoginModel {
-    //criando o método construtor
-    constructor() {}
-    isLogged(){
-        return "Logado";
-    };
-};
+// module.exports = class LoginModel {
+//     //criando o método construtor
+//     constructor() {}
+//     isLogged(){
+//         return "Logado";
+//     };
+// };
